6c2714d70f3a65c0bc782f0028c9e47c06cecba2
Frida <lindstromfrida@yahoo.se>
2020-03-12 19:39:52 +0100

    ´det blev fel

---
+++ src/Component/HandleFileDots.js
@@ -0,0 +1,42 @@
+
+import React, { useEffect, useState } from 'react';
+import {favorites$} from '../store';
+
+
+export default function HandleFileDots({onClickDelete, onClickStar, file, onClickRename, onClickCopy, onClickMove, onClose}){
+    const [favorites , setFavorites] = useState(favorites$.value);
+
+    useEffect(() => {
+        const subscription = favorites$.subscribe(setFavorites);
+        return () => subscription.unsubscribe();
+    },[]);
+
+    return(
+        <div className='modalFileDots' style={{position: 'absolute', zIndex: '10'}}>
+            <ul className='fileDots' style={{backgroundColor: '#F2F2F2', listStyle: 'none', cursor: 'pointer', width: '140px', height: '125px', borderRadius: '5px'}}>
+                <li onClick= {(e) => {
+                    onClickDelete(e);
+                    onClose();
+                }}>Ta bort</li>
+                <li onClick = {(e) => {
+                    onClickRename(e);
+                    onClose();
+                }}>Byt Namn</li>
+                <li onClick = {(e) => {
+                    onClickCopy(e);
+                    onClose();
+                }}>Kopiera</li>
+                {!favorites.find(x => x.id === file.id) ?
+                    <li onClick={(e) => {
+                        onClickStar(e);
+                        onClose();
+                        }}>Stjärnmarkera</li> :
+                    <li onClick={ (e) => {
+                        onClickStar(e);
+                        onClose();
+                    }}>Avmarkera stjärna</li>
+                }
+            </ul>
+        </div>
+    )
+}
